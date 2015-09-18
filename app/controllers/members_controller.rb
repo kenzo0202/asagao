8546250589828bed4c935581d86dd2f815a3df58
tyk2013 <ken.rad0202@gmail.com>
2015-09-18 23:25:56 +0900

    first_commit

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,26 @@
+class MembersController < ApplicationController
+    def index
+        @members = Member.order("number")
+    end
+    def show
+        @member = Member.find(params[:id])
+    end
+    def new
+        @member= Member.new(birthday: Date.new(1980,1,1))
+    end
+    def edit
+        @member= Member.find(params[:id])
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
+    def search
+        @members =Member.search(params[:q])
+        render "index"
+    end
+    
+    
+end
