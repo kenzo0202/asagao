9e80a52dcf88b361e4439d17876c0631a3df5f4b
tyk2013 <ken.rad0202@gmail.com>
2015-10-27 03:36:54 +0900

    test

---
+++ db/seeds/development/articles.rb
@@ -0,0 +1,12 @@
+body=
+    "私は岡野健三です。早稲田大学法学部三年生です"
+
+0.upto(9)do |idx|
+    Article.create(
+        title: "試合の結果#{idx}",
+        body: body,
+        released_at: 8.days.ago.advance(days: idx),
+        expired_at: 2.days.ago.advance(days: idx),
+        member_only: (idx % 3 ==0)
+        )
+end
